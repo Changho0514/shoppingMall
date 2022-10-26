e55cce00ca5e3af86a8fbb093e9a78654f1ff19b
Changho0514 <chh9450@naver.com>
2022-10-26 19:48:51 +0900

    object relation mapping

---
+++ src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "cart")
+public class Cart extends BaseEntity {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
     @Id
     @Column(name = "item_id")
     @GeneratedValue
@@ -34,6 +34,4 @@ public class Item {
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
 
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
 }
